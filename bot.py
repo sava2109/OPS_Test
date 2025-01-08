375d95d8b31e5cb717383536c5adab30bd578093
Salmen Abbes <114873030+Salmen-Abbes@users.noreply.github.com>
2025-01-08 14:09:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
 import asyncio
-from datetime import datetime
+from datetime import datetime,timedelta
 
 import os
 from dotenv import load_dotenv
@@ -15,43 +15,43 @@ from app.periodic_tests.clicup_tickets import clickup_State_Machine
 TRX_STATE_MACHINE = Trx_State_Machine()
 CLICKUP_STATE_MACHINE = clickup_State_Machine()
 async def routine_checks_month(bot):
-
     while True:
         try:
+            print(f"Running monthly task at {datetime.now()}")
             await CLICKUP_STATE_MACHINE.update()
         except Exception as e:
-            print(f"Error in hourly task: {e}")
-        now = datetime.now()
-        next_hour = now.replace(minute=0, second=0, microsecond=0)
-        if now >= next_hour:
-            next_hour = next_hour.replace(hour=next_hour.month + 1)
-        
-        wait_time = (next_hour - now).total_seconds()
-        await asyncio.sleep(wait_time)
-async def routine_checks_hour(bot):
+            print(f"Error in monthly task: {e}")
 
+        now = datetime.now()
+        next_month = (now.replace(day=1) + timedelta(days=32)).replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+        wait_time = (next_month - now).total_seconds()
+        await asyncio.sleep(wait_time)
+
+
+async def routine_checks_hour(bot):
     while True:
         try:
-            print(f"Running hourly task at {datetime.now()} of Checking Tickets, bot Messages , Terminal Balances")
+            print(f"Running hourly task at {datetime.now()}")
             await CLICKUP_STATE_MACHINE.update()
             await check_balance(bot)
-            # await check_pending_messages(bot)
         except Exception as e:
             print(f"Error in hourly task: {e}")
+
         now = datetime.now()
-        next_hour = now.replace(minute=0, second=0, microsecond=0)
-        if now >= next_hour:
-            next_hour = next_hour.replace(hour=next_hour.hour + 1)
-        
+        next_hour = (now + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
         wait_time = (next_hour - now).total_seconds()
         await asyncio.sleep(wait_time)
+
+
 async def main():
     load_dotenv()
     bot = Bot(token=os.getenv('TOKEN_DEV'))
     await asyncio.gather(
         run_bot(bot),
         run_trx_state_machine(bot),
-        routine_checks(bot)
+        routine_checks_hour(bot),
+        routine_checks_month(bot)
     )
 async def run_bot(bot):
     dp = Dispatcher()
